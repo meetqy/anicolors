c71535d83a791aa377f7b9525b0dac74166ed4ab
meetqy <meetqy@qq.com>
2025-07-01 15:34:30 +0800

    feat: update page title for Dori color palette maker and adjust layout for better responsiveness

--- src/app/palette/[slug]/page.tsx
+++ src/app/palette/[slug]/page.tsx
@@ -3,8 +3,8 @@ import { Generator } from "~/app/components/generator";
 export default function Page() {
   return (
     <div className="mx-auto py-12">
-      <div className="mx-auto mb-12 max-w-screen-lg">
-        <h1 className="h1 text-left">Create Your 5-Color Palette for Dori</h1>
+      <div className="mx-auto mb-12 max-w-screen-lg px-4 lg:px-0">
+        <h1 className="h1 text-left">Genshin Impact Dori Color Palette Maker</h1>
         <p className="p">
           Click and drag the color points on the image to select five colors you feel best represent Dori. Everyone sees colors differently — express
           your version of this character through your own custom palette!
