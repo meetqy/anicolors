69ac246f1c4980f77525bc716541eddbec4cb1dd
meetqy <meetqy@qq.com>
2025-07-03 22:16:54 +0800

    feat: enhance topic metadata generation and improve page structure for dynamic content

--- nextjs/app/topic/[id]/page.tsx
+++ nextjs/app/topic/[id]/page.tsx
@@ -25,21 +25,46 @@ const getData = async (id: string) => {
     },
   });
 
-  return res.data.topic;
+  return res.data.topic as {
+    name: string;
+    category: string;
+    image: { url: string };
+    palettes: { points: ColorPoint[] }[];
+  };
+};
+
+export const generateMetadata = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const topic = await getData(id);
+  return {
+    title: `Color Palette by ${topic.name} | HiColors`,
+    description: `Create a custom color palette for ${topic.name} from the ${topic.category} category.`,
+    openGraph: {
+      images: [
+        {
+          url: "http://localhost:1337" + topic.image.url,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
 };
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const topic = await getData(id);
-  const points = topic.palettes[0].points as ColorPoint[];
+  const points = topic.palettes[0].points;
 
   return (
     <div className="mx-auto py-12">
       <div className="mx-auto mb-12 max-w-screen-lg px-4 lg:px-0">
-        <h1 className="h1 text-left">Genshin Impact Dori Color Palette Maker</h1>
+        <h1 className="h1 text-left capitalize">
+          {topic.category} {topic.name} Color Palette Maker
+        </h1>
         <p className="p">
-          Click and drag the color points on the image to select five colors you feel best represent Dori. Everyone sees colors differently — express your version of this character through your own
-          custom palette!
+          Click and drag the color points on the image to select five colors you feel best represent <b>{topic.name}</b>. Everyone sees colors differently — express your version of this character
+          through your own custom palette!
         </p>
       </div>
       <Generator initialPoints={points} initImage={"http://localhost:1337" + topic.image.url} />
