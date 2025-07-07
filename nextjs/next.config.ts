1c525279722d1839d097531daf89d52d1ddb08d9
meetqy <meetqy@qq.com>
2025-07-07 17:22:32 +0800

    fix: add remote pattern for R2 image hosting in next.config.ts

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("http://localhost:1337/**/*")],
+    remotePatterns: [new URL("http://localhost:1337/**/*"), new URL("https://r2.hicolors.org/**/*")],
   },
 };
 
