fb07345f2cf1346b33f80bd730946128e3c3b5f6
meetqy <meetqy@qq.com>
2025-07-08 20:37:09 +0800

    fix: refactor PaletteCard component for improved background color handling and hover effects

--- nextjs/app/palettes/components/palette-card.tsx
+++ nextjs/app/palettes/components/palette-card.tsx
@@ -8,10 +8,27 @@ interface PaletteCardProps {
 }
 
 export const PaletteCard = ({ palette }: PaletteCardProps) => {
+  const bgColor = palette.points[0].color;
+
   return (
     <div className="relative ">
-      <Link href={`/palettes/${palette.documentId}`} className="group">
-        <div className="rounded-md relative" style={{ backgroundColor: palette.points[0].color }}>
+      <Link
+        href={`/palettes/${palette.documentId}`}
+        className="group"
+        style={{
+          perspective: "1000px",
+          transformStyle: "preserve-3d",
+        }}
+      >
+        <div className="rounded-md relative">
+          <div
+            className="absolute bottom-0 left-1/2 w-full h-full rounded-md -translate-x-1/2 transition-all duration-300 group-hover:w-48 group-hover:h-48 group-hover:rounded-full group-hover:bottom-[-70px] group-hover:rotate-x-[75deg]"
+            style={{
+              backgroundColor: bgColor,
+            }}
+          />
+          {/* 额外的流畅背景层 */}
+          <div style={{ backgroundColor: bgColor }} className="absolute inset-0 rounded-md opacity-100 group-hover:opacity-0 transition-opacity duration-300" />
           <img
             src={getAssetUrl(palette.image.url, 320)}
             srcSet={`
@@ -20,7 +37,7 @@ export const PaletteCard = ({ palette }: PaletteCardProps) => {
               ${getAssetUrl(palette.image.url, 960)} 3x
             `}
             alt={palette.name}
-            className={cn("w-full z-10 relative aspect-square object-contain group-hover:scale-125 transition-transform")}
+            className={cn("w-full z-10 relative aspect-square object-contain transition-transform duration-300 group-hover:scale-125 group-hover:translate-y-[-50px] group-hover:rotate-x-[10deg]")}
           />
           <div className="flex w-full rounded-b-md overflow-hidden absolute bottom-0 left-0">
             {palette.points.map((point, index) => {
