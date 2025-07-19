151af2c4e7884d998896753acc8f4b214e4e4a84
meetqy <meetqy@qq.com>
2025-07-19 13:39:16 +0800

    refactor: streamline category synchronization logic in lifecycles

--- strapi/src/api/palette/content-types/palette/lifecycles.ts
+++ strapi/src/api/palette/content-types/palette/lifecycles.ts
@@ -1,15 +1,12 @@
 const syncCategory = async (event) => {
-  const { connect } = event.params.data.categoryExtend;
-  const categoryId = connect?.[0]?.id;
-
-  if (!categoryId) {
-    return;
+  const categoryExtend = event.params.data.categoryExtend;
+  const categoryId = categoryExtend?.connect?.[0]?.id;
+  if (categoryId) {
+    const category = await strapi.db.query("api::category.category").findOne({
+      where: { id: categoryId },
+    });
+    event.params.data.category = category.name;
   }
-
-  const category = await strapi.db.query("api::category.category").findOne({
-    where: { id: categoryId },
-  });
-  event.params.data.category = category.name;
 };
 
 export default {
