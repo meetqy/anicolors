a1fc09eaded7c458480cc355d79ed2cba47d8473
meetqy <meetqy@qq.com>
2025-07-19 13:32:35 +0800

    refactor: extract category synchronization logic into a separate function

--- strapi/src/api/palette/content-types/palette/lifecycles.ts
+++ strapi/src/api/palette/content-types/palette/lifecycles.ts
@@ -1,25 +1,23 @@
+const syncCategory = async (event) => {
+  const { connect } = event.params.data.categoryExtend;
+  const categoryId = connect[0].id;
+
+  if (!categoryId) {
+    return;
+  }
+
+  const category = await strapi.db.query("api::category.category").findOne({
+    where: { id: categoryId },
+  });
+  event.params.data.category = category.name;
+};
+
 export default {
-  beforeCreate(event) {
-    const { data, where, select, populate } = event.params;
-    console.log("beforeCreate", event.params);
-
-    // event.params.data.category =
-
-    // let's do a 20% discount everytime
-    // event.params.data.price = event.params.data.price * 0.8;
+  async beforeCreate(event) {
+    await syncCategory(event);
   },
 
   async beforeUpdate(event) {
-    const { connect } = event.params.data.categoryExtend;
-    const categoryId = connect[0].id;
-
-    if (!categoryId) {
-      return;
-    }
-
-    const category = await strapi.db.query("api::category.category").findOne({
-      where: { id: categoryId },
-    });
-    event.params.data.category = category.name;
+    await syncCategory(event);
   },
 };
