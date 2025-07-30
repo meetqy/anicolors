a2a9e5187dc38cfec0f4b938d8ffbca3118a5804
meetqy <meetqy@qq.com>
2025-07-30 13:51:53 +0800

    feat: configure image remote patterns, add robots.txt, and update Toaster import type

--- nextjs-t3/next.config.js
+++ nextjs-t3/next.config.js
@@ -5,6 +5,13 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      new URL("http://localhost:1337/**/*"),
+      new URL("https://r2.hicolors.org/**/*"),
+    ],
+  },
+};
 
 export default config;
