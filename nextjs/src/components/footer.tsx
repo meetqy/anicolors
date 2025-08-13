0d36d5348ac4cdf56163ec889b330b0f5b152f5e
meetqy <meetqy@qq.com>
2025-08-13 22:09:11 +0800

    x

--- nextjs/src/components/footer.tsx
+++ nextjs/src/components/footer.tsx
@@ -31,6 +31,7 @@ export const Footer = () => {
     { name: "AiFinder", url: "https://aifinder.site" },
     { name: "Startupslab", url: "https://startupslab.site" },
     { name: "What Is Ai Tools", url: "https://whatisaitools.com/" },
+    { name: "All in AI Tools", url: "https://allinai.tools" },
   ];
 
   return (
