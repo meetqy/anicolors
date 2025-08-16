b325f5f40534953fa85bbb051006879d31e751f3
meetqy <meetqy@qq.com>
2025-08-16 15:56:16 +0800

    test

--- nextjs/src/app/tools/change-svg-color/page.tsx
+++ nextjs/src/app/tools/change-svg-color/page.tsx
@@ -25,27 +25,27 @@ const getData = async () => {
   return res.data.tools[0]!;
 };
 
-export const generateMetadata = async (): Promise<Metadata> => {
-  const tool = await getData();
-  const images = tool.cover ? [getAssetUrl(tool.cover.url, 1200)] : [];
+// export const generateMetadata = async (): Promise<Metadata> => {
+//   const tool = await getData();
+//   const images = tool.cover ? [getAssetUrl(tool.cover.url, 1200)] : [];
 
-  return {
-    title: tool.name,
-    description: tool.description,
-    twitter: { card: "summary_large_image", images },
-    openGraph: { images },
-    alternates: {
-      canonical: `/tools/${tool.slug}`,
-    },
-  };
-};
+//   return {
+//     title: tool.name,
+//     description: tool.description,
+//     twitter: { card: "summary_large_image", images },
+//     openGraph: { images },
+//     alternates: {
+//       canonical: `/tools/${tool.slug}`,
+//     },
+//   };
+// };
 
 export default async function Page() {
-  const tool = await getData();
+  // const tool = await getData();
 
   return (
     <>
-      <ToolHero tool={tool} />
+      {/* <ToolHero tool={tool} /> */}
 
       <div className="from-muted/20 to-muted/0 text-card-foreground container rounded-lg bg-gradient-to-b lg:border">
         <Generator />
