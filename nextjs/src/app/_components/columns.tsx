bba5b609789bcd19d92f8affaca7335d6c9ada0a
meetqy <meetqy@qq.com>
2025-08-22 11:07:25 +0800

    feat: update Columns component to include script and improve palette rendering

--- nextjs/src/app/_components/columns.tsx
+++ nextjs/src/app/_components/columns.tsx
@@ -5,10 +5,24 @@ import { PaletteCard } from "@/app/palettes/components/palette-card";
 
 export const Columns = ({ palettes }: { palettes: PaletteListItem[] }) => {
   return (
-    <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {palettes.map((palette) => (
-        <PaletteCard key={palette.documentId} palette={palette} />
-      ))}
-    </div>
+    <>
+      <script
+        async
+        data-cfasync="false"
+        src="//immoderatebreathtaking.com/31eff1937586d0b5565373f161001d47/invoke.js"
+      />
+      <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {palettes.slice(0, 8).map((palette) => (
+          <PaletteCard key={palette.documentId} palette={palette} />
+        ))}
+        <div
+          id="container-31eff1937586d0b5565373f161001d47"
+          className="bg-muted w-full rounded-md md:col-span-2 lg:col-span-3 xl:col-span-4"
+        />
+        {palettes.slice(8).map((palette) => (
+          <PaletteCard key={palette.documentId} palette={palette} />
+        ))}
+      </div>
+    </>
   );
 };
