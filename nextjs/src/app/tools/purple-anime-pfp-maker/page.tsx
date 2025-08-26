fea1bb4da986af8fdad4b9d760483c5e311d4527
meetqy <meetqy@icloud.com>
2025-08-26 17:41:40 +0800

    feat: add generator tool for creating purple-themed anime profile pictures and implement switch component

---
+++ nextjs/src/app/tools/purple-anime-pfp-maker/page.tsx
@@ -0,0 +1,36 @@
+import { Generator } from "./generator";
+import type { Metadata } from "next";
+import { getAssetUrl } from "@/lib/utils";
+import { ToolHero } from "@/components/tool-hero";
+import { getToolData } from "../_utils";
+
+const slug = "purple-anime-pfp-maker";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const tool = await getToolData(slug);
+  const images = tool.cover ? [getAssetUrl(tool.cover.url, 1200)] : [];
+
+  return {
+    title: tool.name,
+    description: tool.description,
+    twitter: { card: "summary_large_image", images },
+    openGraph: { images },
+    alternates: {
+      canonical: `/tools/${tool.slug}`,
+    },
+  };
+};
+
+export default async function Page() {
+  const tool = await getToolData(slug);
+
+  return (
+    <>
+      <ToolHero tool={tool} />
+
+      <div className="from-muted/20 to-muted/0 text-card-foreground container rounded-lg bg-gradient-to-b lg:border">
+        <Generator />
+      </div>
+    </>
+  );
+}
