641b3a33db429a13ea51378d81d960f5e60a13f2
meetqy <meetqy@icloud.com>
2025-09-01 17:21:14 +0800

    disable

--- nextjs/src/app/robots.ts
+++ nextjs/src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/palettes"],
     },
     sitemap: `${env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
